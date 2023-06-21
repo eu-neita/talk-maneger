52a7e06dface52e297287e5b398e08e308a7785a
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-06-21 14:32:54 -0300

    fix: refatora código

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -4,6 +4,17 @@ const talkerDB = require('./db/talkerDB');
 
 const router = express.Router();
 
+router.get('/search', async (req, res) => {
+  const tokenHeader = req.headers.authorization;
+  if (!tokenHeader) return res.status(401).json({ message: 'Token não encontrado' });
+  if (tokenHeader.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  const searchTerm = req.query.q;
+  const result = await talkerDB.searchTerm(searchTerm);
+  res.status(200).json(result);
+});
+
 router.get('/', async (req, res) => {
   try {
     const result = await talkerDB.findAll();
@@ -14,19 +25,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const result = await talkerDB.findById(id);
-//     if (!result || result.length === 0) {
-//     res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
-//     }
-//     res.status(200).json(result);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: `Esse erro ocorreu ao encontrar uma tarefa: ${err}` });
-//   }
-// });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await talkerDB.findById(id);
+    if (!result || result.length === 0) {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' }); 
+    }
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Esse erro ocorreu ao encontrar uma tarefa: ${err}` });
+  }
+});
 
 router.post('/', async (req, res) => {
   const talkerList = req.body;
@@ -79,18 +90,6 @@ router.delete('/:id', async (req, res) => {
     console.log(err);
     res.status(500).json({ message: `Esse erro ocorreu ao encontrar uma tarefa: ${err}` });
   }
-});
-
-router.get('/search', async (req, res) => {
-  const tokenHeader = req.headers.authorization;
-  if (!tokenHeader) return res.status(401).json({ message: 'Token não encontrado' });
-  if (tokenHeader.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
-  const searchTerm = req.query.q;
-  const result = await talkerDB.searchTerm(searchTerm);
-  res.status(200).json(result);
-  // res.status(200).json({ message: 'Busca realizada com sucesso' });
 });
 
 module.exports = router;
